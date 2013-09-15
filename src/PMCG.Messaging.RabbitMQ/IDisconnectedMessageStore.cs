229cad6577d51ab3f81f88f9620b3bc2d560df97
pmcgrath <pmcgrath@gmail.com>
2013-09-15 20:49:49 +0100

    Introduced disconnected store interface

---
+++ src/PMCG.Messaging.RabbitMQ/IDisconnectedMessageStore.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace PMCG.Messaging.RabbitMQ
+{
+	public interface IDisconnectedMessageStore
+	{
+		IEnumerable<Guid> GetAllIds();
+
+		string Add(
+			Message message);
+
+		Message Get(
+			Guid id);
+
+		void Delete(
+			Guid id);
+	}
+}
