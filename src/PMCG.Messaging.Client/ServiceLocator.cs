ec006aa8c38a5b196d00ac7d0fd75868b8540b2a
pmcgrath <pmcgrath@gmail.com>
2013-09-17 20:45:41 +0100

    Added a service locator

---
+++ src/PMCG.Messaging.Client/ServiceLocator.cs
@@ -0,0 +1,22 @@
+﻿using PMCG.Messaging.Client.Configuration;
+using PMCG.Messaging.Client.DisconnectedStorage;
+using PMCG.Messaging.Client.Utility;
+using RabbitMQ.Client;
+using System;
+
+
+namespace PMCG.Messaging.Client
+{
+	public static class ServiceLocator
+	{
+		public static Func<ILog> GetLogger =
+			() => new ConsoleLogger();
+
+		public static Func<ILog, BusConfiguration, IConnectionManager> GetConnectionManager =
+			(logger, busConfiguration) => new ConnectionManager(logger, busConfiguration.ConnectionUri, busConfiguration.ReconnectionPauseInterval);
+
+
+		public static Func<BusConfiguration, IStore> GetNewDisconnectedStore = 
+			(busConfiguration) => new FileSystemStore(busConfiguration.DisconnectedMessagesStoragePath);
+	}
+}
