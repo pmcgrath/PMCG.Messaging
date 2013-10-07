9c7d901a1c926ca51949af60f8793e3a74610850
pmcgrath <pmcgrath@gmail.com>
2013-10-07 11:18:54 +0100

    Added build script

---
+++ build.ps1
@@ -0,0 +1,8 @@
+# Output path
+$outputDirectoryPath = join-path (split-path $MyInvocation.MyCommand.Path) 'bin'
+
+# Build
+c:\windows\microsoft.net\framework64\v4.0.30319\msbuild.exe src\PMCG.Messaging.Client\PMCG.Messaging.Client.csproj `
+	/target:ReBuild `
+	/property:Configuration=Release `
+	/property:OutputPath=$outputDirectoryPath
