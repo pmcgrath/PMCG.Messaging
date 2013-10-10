a2c643dfabb8f6916271e251a3bfc91825188401
pmcgrath <pmcgrath@gmail.com>
2013-10-10 07:39:11 +0100

    Added DPAPI password renewal issue info

--- src/PMCG.Messaging.Client/Configuration/DefaultPasswordParser.cs
+++ src/PMCG.Messaging.Client/Configuration/DefaultPasswordParser.cs
@@ -6,6 +6,21 @@ using System.Text;
 
 namespace PMCG.Messaging.Client.Configuration
 {
+	/*
+		Can use the following powershell at each machine to generate the user's password on the machine (Will need to run as the specific user)
+			# Required for cryptography types
+			[System.Reflection.Assembly]::LoadWithPartialName("System.Security") | out-null;
+
+			$text = 'ThePassword';
+			$cipherText = [Convert]::ToBase64String([System.Security.Cryptography.ProtectedData]::Protect([System.Text.Encoding]::UTF8.GetBytes($text), $null, 'CurrentUser'));
+			$decryptedText = [System.Text.Encoding]::UTF8.GetString([System.Security.Cryptography.ProtectedData]::Unprotect([Convert]::FromBase64String($cipherText), $null, 'CurrentUser'));
+
+			write-host ("{0,-20}{1}" -f 'Text', $text);
+			write-host ("{0,-20}{1}" -f 'Cipher', $cipherText);
+			write-host ("{0,-20}{1}" -f 'Decrypted', $decryptedText);
+		See http://msdn.microsoft.com/en-us/library/system.security.cryptography.protecteddata.aspx
+			If the user's password is changed, this will prevent the decryption of the password generated from the above domain account
+	*/
 	public class DefaultPasswordParser : IPasswordParser
 	{
 		public string Parse(
