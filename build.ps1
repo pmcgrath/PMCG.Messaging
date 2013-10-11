6b1049f5f5e849d606af79e87b9ff110933c137e
pmcgrath <pmcgrath@gmail.com>
2013-10-11 11:48:33 +0100

    Added result line to build output

--- build.ps1
+++ build.ps1
@@ -5,4 +5,7 @@ $outputDirectoryPath = join-path (split-path $MyInvocation.MyCommand.Path) 'bin'
 c:\windows\microsoft.net\framework64\v4.0.30319\msbuild.exe src\PMCG.Messaging.Client\PMCG.Messaging.Client.csproj `
 	/target:ReBuild `
 	/property:Configuration=Release `
-	/property:OutputPath=$outputDirectoryPath
+	/property:OutputPath=$outputDirectoryPath;
+
+# Result
+write-host "`n`n`n*****`nBinaries are available @ $outputDirectoryPath`n*****`n"
