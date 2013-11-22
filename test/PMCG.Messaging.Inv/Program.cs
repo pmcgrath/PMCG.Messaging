a41e4bdc57dc48346867970e9334108157be9296
pmcgrath <pmcgrath@gmail.com>
2013-11-22 22:35:44 +0000

    Added more tpl investigations

--- test/PMCG.Messaging.Inv/Program.cs
+++ test/PMCG.Messaging.Inv/Program.cs
@@ -8,13 +8,15 @@ namespace PMCG.Messaging.Inv
 		static void Main(
 			string[] args)
 		{
-			PublisherConfirmsInv.Run(null);
-			PublisherConfirmsWithTasksInv.Run(null);
+			//PublisherConfirmsInv.Run(null);
+			//PublisherConfirmsWithTasksInv.Run(null);
 			//new PublisherConfirmsUsage().Run(50000);
 			//new SyncPublisherConfirms().Run(500);
 			//new TopicsUsage().Run();
 			//new TaskInv().RunCase1();
 			//new TaskInv().RunCase2();
+			//new TaskInv2().RunCase();
+			new TaskInv2().RunCase2();
 		}
 	}
 }
--- test/PMCG.Messaging.Inv/TaskInv2.cs
+++ test/PMCG.Messaging.Inv/TaskInv2.cs
@@ -7,42 +7,9 @@ using System.Threading.Tasks;
 
 namespace PMCG.Messaging.Inv
 {
-	public class TaskInv
+	public class TaskInv2
 	{
-		public void RunCase1()
-		{
-			var _tasks = new []
-				{
-					new Task<Task<bool>>(() => this.DoWork(1, TimeSpan.FromMilliseconds(50), false)),
-					new Task<Task<bool>>(() => this.DoWork(2, TimeSpan.FromMilliseconds(25), false)),
-					new Task<Task<bool>>(() => this.DoWork(3, TimeSpan.FromMilliseconds(50), true)),
-					new Task<Task<bool>>(() => this.DoWork(4, TimeSpan.FromMilliseconds(20), false)),
-					new Task<Task<bool>>(() => this.DoWork(5, TimeSpan.FromMilliseconds(20), false)),
-					new Task<Task<bool>>(() => this.DoWork(6, TimeSpan.FromMilliseconds(20), false)),
-					new Task<Task<bool>>(() => this.DoWork(7, TimeSpan.FromMilliseconds(20), false)),
-					new Task<Task<bool>>(() => this.DoWork(8, TimeSpan.FromMilliseconds(20), false)),
-					new Task<Task<bool>>(() => this.DoWork(9, TimeSpan.FromMilliseconds(20), true))
-				};
-
-			try
-			{
-				Console.WriteLine("{0:mm:ss.ffffff} [{1}] About to start", DateTime.Now, Thread.CurrentThread.ManagedThreadId);
-				Parallel.ForEach(_tasks, task => task.Start());
-				Console.WriteLine("{0:mm:ss.ffffff} [{1}] Waiting on tasks", DateTime.Now, Thread.CurrentThread.ManagedThreadId);
-				Task.WaitAll(_tasks);
-				Console.WriteLine("{0:mm:ss.ffffff} [{1}] Tasks completed !", DateTime.Now, Thread.CurrentThread.ManagedThreadId);
-			}
-			catch (Exception exception)
-			{
-				Console.WriteLine("{0:mm:ss.ffffff} [{1}] Error : {2} {3}", DateTime.Now, Thread.CurrentThread.ManagedThreadId, exception.GetType(), exception.Message);
-			}
-
-			Console.WriteLine("{0:mm:ss.ffffff} [{1}] Hit enter to exit", DateTime.Now, Thread.CurrentThread.ManagedThreadId);
-			Console.ReadLine();
-		}
-
-
-		public void RunCase2()
+		public void RunCase()
 		{
 			try
 			{
@@ -73,24 +40,73 @@ namespace PMCG.Messaging.Inv
 		}
 
 
+		public void RunCase2()
+		{
+			try
+			{
+				Console.WriteLine("{0:mm:ss.ffffff} [{1}] About to invoke", DateTime.Now, Thread.CurrentThread.ManagedThreadId);
+				var _invokeResult = this.RunMultipleTasksWithSingleResult();
+
+				Console.WriteLine("{0:mm:ss.ffffff} [{1}] About to wait", DateTime.Now, Thread.CurrentThread.ManagedThreadId);
+				if (!_invokeResult.Wait(TimeSpan.FromSeconds(20)))
+				{
+					Console.WriteLine("{0:mm:ss.ffffff} [{1}] Timed out !", DateTime.Now, Thread.CurrentThread.ManagedThreadId);
+				}
+				Console.WriteLine("{0:mm:ss.ffffff} [{1}] Done waiting", DateTime.Now, Thread.CurrentThread.ManagedThreadId);
+				if (_invokeResult.IsCompleted)
+				{
+					Console.WriteLine("{0:mm:ss.ffffff} [{1}]        Result is {2}", DateTime.Now, Thread.CurrentThread.ManagedThreadId, _invokeResult.Result);
+				}
+			}
+			catch (Exception exception)
+			{
+				Console.WriteLine("{0:mm:ss.ffffff} [{1}] Error : {2} {3}", DateTime.Now, Thread.CurrentThread.ManagedThreadId, exception.GetType(), exception.Message);
+			}
+
+			Console.WriteLine("{0:mm:ss.ffffff} [{1}] Hit enter to exit", DateTime.Now, Thread.CurrentThread.ManagedThreadId);
+			Console.ReadLine();
+		}
+
+
 		public Task<bool[]> RunMultipleTasks()
 		{
-			var _tasks = new[]
+			var _tasks = new []
 				{
-					this.DoWork(1, TimeSpan.FromMilliseconds(5000), false),
-					this.DoWork(2, TimeSpan.FromMilliseconds(25), false),
-					this.DoWork(3, TimeSpan.FromMilliseconds(50), false)
+					this.DoWork(1, TimeSpan.FromMilliseconds(5000), resultToReturn: true, shouldThrowException: false),
+					this.DoWork(2, TimeSpan.FromMilliseconds(25), resultToReturn: true, shouldThrowException: false),
+					this.DoWork(3, TimeSpan.FromMilliseconds(50), resultToReturn: true, shouldThrowException: false),
 				};
 
-			var _result = Task.WhenAll(_tasks);
+			return Task.WhenAll(_tasks);
+		}
 
-			return _result;
+
+		public Task<bool> RunMultipleTasksWithSingleResult()
+		{
+			var _result = new TaskCompletionSource<bool>();
+
+			var _tasks = new[]
+				{
+					this.DoWork(1, TimeSpan.FromMilliseconds(5000), resultToReturn: true, shouldThrowException: false),
+					this.DoWork(2, TimeSpan.FromMilliseconds(25), resultToReturn: false, shouldThrowException: false),
+					this.DoWork(3, TimeSpan.FromMilliseconds(50), resultToReturn: true, shouldThrowException: false),
+				};
+
+			Task.WhenAll(_tasks).ContinueWith(taskResults =>
+				{
+					Console.WriteLine("{0:mm:ss.ffffff} [{1}] ---> Processing result", DateTime.Now, Thread.CurrentThread.ManagedThreadId);
+					var _allGood = taskResults.Result.All(result => result);
+					_result.SetResult(_allGood);
+				});
+
+			return _result.Task;
 		}
 
 
 		private Task<bool> DoWork(
 			int sequence,
 			TimeSpan sleepInterval,
+			bool resultToReturn,
 			bool shouldThrowException)
 		{
 			Console.WriteLine("{0:mm:ss.ffffff} [{1}] Starting for sequence {2}", DateTime.Now, Thread.CurrentThread.ManagedThreadId, sequence);
@@ -104,7 +120,7 @@ namespace PMCG.Messaging.Inv
 				throw new ApplicationException("Bang !");
 			}
 
-			_result.SetResult(true);
+			_result.SetResult(resultToReturn);
 			Console.WriteLine("{0:mm:ss.ffffff} [{1}] Completed for sequence {2}", DateTime.Now, Thread.CurrentThread.ManagedThreadId, sequence);
 
 			return _result.Task;
