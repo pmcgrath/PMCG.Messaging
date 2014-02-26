7f0f0f9a10115571eeea379a916485d6537b9e34
pmcgrath <pmcgrath@gmail.com>
2014-02-26 22:21:43 +0000

    Added a timestamp to command messages and altered message delivery type to allow direct exchange publications

---
+++ test/PMCG.Messaging.Client.UT/Configuration/MessageDeliverySpec.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using PMCG.Messaging.Client.Configuration;
+using System;
+
+
+namespace PMCG.Messaging.Client.UT.Configuration
+{
+	[TestFixture]
+	public class MessageDeliverySpec
+	{
+		[Test]
+		public void Ctor_Cater_For_Direct_Exchange_Delivery()
+		{
+			var _SUT = new MessageDelivery(
+				string.Empty,
+				"TypeHeader",
+				MessageDeliveryMode.Persistent,
+				message => "DirectQueue");
+
+			Assert.AreEqual(string.Empty, _SUT.ExchangeName);
+			Assert.AreEqual("TypeHeader", _SUT.TypeHeader);
+			Assert.AreEqual(MessageDeliveryMode.Persistent, _SUT.DeliveryMode);
+			Assert.AreEqual("DirectQueue", _SUT.RoutingKeyFunc(null));
+		}
+	}
+}
